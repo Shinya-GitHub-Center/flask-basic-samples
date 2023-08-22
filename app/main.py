7466ffd81001c0e7f128069bebd4cf58257134e5
Shinya Ihara <129726604+Shinya-GitHub-Center@users.noreply.github.com>
2023-08-22 15:28:25 +0900

    sync with database mod #1

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import select
+from app import models
 
 
 app = Flask(__name__)
@@ -18,9 +20,11 @@ Migrate(app, db, directory=mgdir)
 
 @app.route('/')
 def index():
-    name = 'index'
-    return render_template('index.html', title='This is the index page', name=name)
+    stmt = select(models.Blogpost).order_by(models.Blogpost.id.desc())
+    entries = db.session.execute(stmt).scalars().all()
+    return render_template('index.html', rows=entries)
 
 
 from app.crud.views import crud
+
 app.register_blueprint(crud)
